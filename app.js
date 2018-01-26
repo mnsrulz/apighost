4fd0f91c0ee128aebf42de1d15a5ae1ff64fc841
manishrawat4u <manishrawat4u@gmail.com>
2018-01-27 01:24:53 +0530

    imported gddirecturl as npm package (#3)

--- app.js
+++ app.js
@@ -1,53 +1,19 @@
-
 var restify = require('restify');
-const got = require('got');
-const cheerio = require('cheerio');
-const cookie = require('cookie');
-var setCookie = require('set-cookie-parser');
+var gddirect = require('gddirecturl');
 async function gdriveHandler(req, res, next) {
     try {
-
-        var gd3 = got.get('https://drive.google.com/thumbnail?sz=w320&id=1pB9y4X4qIgCHgDkxBjcWGKgLiZpT0WVZ', {
-            followRedirect: false
-        });
-
-        var gd1 = await got.get('https://drive.google.com/uc?id=' + req.params.gdriveid + '&export=download');
-
-        var responseCookies = setCookie.parse(gd1.headers["set-cookie"]);
-        var nextRequestCookies = responseCookies.map((x) => {
-            return x.domain == '.drive.google.com' ? cookie.serialize(x.name, x.value) : null;
-        }).join(';');
-
-        const $ = cheerio.load(gd1.body);
-
-        var downloadLink = $('#uc-download-link').attr('href');
-        var gd2 = await got.get('https://drive.google.com' + downloadLink, {
-            headers: {
-                cookie: nextRequestCookies
-            }, followRedirect: false
-        });
-        var o = gd2.headers.location;
-    var thumbResponse=    await gd3;
-        var o1 = thumbResponse.headers.location;
-        res.send({
-            link: o,
-            thumbnail: o1
-        });
-        return;
+        var o = await gddirect.getMediaLink(req.params.gdriveid);
+        res.send(o);
     } catch (error) {
-        res.send('error'); return;
+        res.send('error');
     }
-
-    //res.send('hello ' + req.params.name);
-    next();
 }
 
 var server = restify.createServer();
 server.get('/api/gddirect/:gdriveid', gdriveHandler);
-server.get('/',function(req,res){
+server.get('/', function (req, res) {
     res.send('Welcome to api ghost!!!');
 });
-// server.head('/hello/:name', respond);
 
 var port = normalizePort(process.env.PORT || '4000');
 
