f2b604d7b9ebff0cfd03a7d4ae16d127ea57de89
Manish Rawat <manish.rawat@nagarro.com>
2018-11-22 22:09:53 -0500

    streaming auto redirect

--- app.js
+++ app.js
@@ -10,28 +10,25 @@ async function gdriveHandler(req, res, next) {
     }
 }
 
-async function bloidmediaplayerHandler(req, res, next){
-	var u = 'https://github.com/manishrawat4u/plugin.video.bloimediaplayer/releases/download/0.1.0/plugin.video.bloimediaplayer-0.1.0.zip';
-	var options = {
-	  host: 'github.com',
-	  port: 443,
-	  path: '/manishrawat4u/plugin.video.bloimediaplayer/releases/download/0.1.0/plugin.video.bloimediaplayer-0.1.0.zip',
-	  method: 'GET',
-	  headers: {
-		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36',
-		'accept-encoding':"gzip, deflate",
-		'Accept':"*/*",
-		'referer':u
-	  }
-	};
-	const request = https.get(options, function(response) {
-        const contentType = response.headers['content-type'];
+async function bloidmediaplayerHandler(req, res, next) {
+    var u = 'https://github.com/manishrawat4u/plugin.video.bloimediaplayer/releases/download/0.1.0/plugin.video.bloimediaplayer-0.1.0.zip';
 
-        console.log(contentType);
+    const request = https.get(u, function (response) {
+        var contentType = response.headers['content-type'];
 
-        res.setHeader('Content-Type', contentType);
-
-        response.pipe(res);
+        if (response.statusCode == 302 || response.statusCode == 301) {
+            var newlocation = response.headers['location'];
+            https.get(newlocation, function (newresponse) {
+                contentType = newresponse.headers['content-type'];
+                res.setHeader('Content-Type', contentType);
+                res.setHeader('content-disposition', newresponse.headers['content-disposition'])
+                newresponse.pipe(res);
+            })
+        }else{
+            console.log(contentType);
+            res.setHeader('Content-Type', contentType);
+            response.pipe(res);
+        }
     });
 }
 
